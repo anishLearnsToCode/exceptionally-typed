4f0a2ad4a77742ddd78e0f48093f65df7163fd4f
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2019-06-19 00:36:26 +0200

    added the string-extention under new ExceptionallyTyped module

--- src/string-extention.ts
+++ src/string-extention.ts
@@ -1,12 +1,14 @@
-export namespace ExceptionallyTyped {
-    export module StringExtension {
-        String.prototype.reverse = function (this: string) {
-            let reversedString = '';
-            for (let index = this.length - 1; index >= 0; index--) {
-                reversedString += this.charAt(index);
-            }
+export module ExceptionallyTyped {
+    String.prototype.reverse = function (this: string) {
+        let reversedString = '';
+        for (let index = this.length - 1; index >= 0; index--) {
+            reversedString += this.charAt(index);
+        }
 
-            return reversedString;
-        };
-    }
+        return reversedString;
+    };
+
+    String.prototype.hello = function () {
+        return 'hi';
+    };
 }
--- src/string.d.ts
+++ src/string.d.ts
@@ -1,5 +1,6 @@
 export declare global {
     interface String {
         reverse(): string;
+        hello(): string;
     }
 }
